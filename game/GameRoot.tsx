03526ca95d6693c510f02ae34ae7f2165918ef2f
Adam <adam.kuzniarski@gmx.de>
2025-12-14 23:34:38 +0100

    add function to gameRoot.tsx

--- game/GameRoot.tsx
+++ game/GameRoot.tsx
@@ -1,3 +1,16 @@
-export default function GameRoot() {
-	return (<div>test1</div>)
-}
+"use client"
+
+
+import { useEffect, useRef, useState } from "react"
+import { GameLayout } from "./components/GameLayout"
+
+import { GAME_CANVAS_HEIGHT_IN_PIXELS, GAME_CANVAS_WIDTH_IN_PIXELS } from "./core/gameConstants"
+
+
+import { createInitialGameState } from "./core/gameFactory"
+import { renderGameStateToCanvas } from "./core/gameRender"
+import { updateGameStateForNextFrame } from "./core/gameUpdate"
+import type { GameState, InputState } from "./core/gameTypes"
+
+export default function GameRoot() { }
+
